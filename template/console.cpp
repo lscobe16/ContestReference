89ad301c29cd914d72cc08a72f6b61dc3ca5f8ad
Linus <linuscodes@scobe.de>
2023-11-06 22:01:53 +0100

    console for Nwerc

--- template/console.cpp
+++ template/console.cpp
@@ -1,2 +1,6 @@
-alias comp="g++ -std=gnu++17 -O2 -Wall -Wextra -Wconversion -Wshadow"
+// mygpp is what the judge uses
+alias mygpp="g++ -std=gnu++20 -x c++ -Wall -O2 -static -pipe -o $1 \"$1.cpp\" -lm"
+
+alias comp="g++ -std=gnu++20 -O2 -Wall -Wextra -Wshadow -Wconversion"
 alias dbg="comp -g -fsanitize=address,undefined"
+// use    comp main.cpp && ./a.out
