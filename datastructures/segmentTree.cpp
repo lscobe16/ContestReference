77dc1c2bc71a225731eb2944ad81b648c4d0c738
florian <uwnte@student.kit.edu>
2023-11-07 15:56:50 +0100

    iterSegT

--- datastructures/segmentTree.cpp
+++ datastructures/segmentTree.cpp
@@ -1,42 +1,46 @@
-vector<ll> tree;
-constexpr ll neutral = 0; // Neutral element for combine
+struct iterSegT {
+    using A = z;
+    A e = 0;
+    A agg(A a, A b) {return a+b;}
 
-ll combine(ll a, ll b) {
-	return a + b;
-}
+    z n;
+    vector<A> v;
 
-void init(vector<ll>& a) {
-	tree.assign(2 * sz(a), 0);
-	copy(all(a), tree.begin() + sz(a));
-	for (int i = sz(tree)/2 - 1; i > 0; i--) {
-		tree[i] = combine(tree[2 * i], tree[2 * i + 1]);
-}}
+    iterSegT(z n): n(n), v(2 * n, e) {}
+    iterSegT(vector<A> s): n(s.size()), v(n, e) {
+        v.insert(v.end(), be(s));
+        for (z i = n; i-- > 0;)
+            v[i] = agg(v[2 * i], v[2 * i + 1]);
+    }
 
-void update(int i, ll val) {
-	for (tree[i += sz(tree)/2] = val; i /= 2; ) {
-		tree[i] = combine(tree[2 * i], tree[2 * i + 1]);
-}}
+    @\yellow{Point updates, Range queries}@
+    void set(z i, A a) {
+        v[i += n] = a;
+        while (i /= 2)
+            v[i] = agg(v[2 * i], v[2 * i + 1]);
+    }
 
-ll query(int l, int r) {
-	ll resL = neutral, resR = neutral;
-	for (l += sz(tree)/2, r += sz(tree)/2; l < r; l /= 2, r /= 2) {
-		if (l&1) resL = combine(resL, tree[l++]);
-		if (r&1) resR = combine(tree[--r], resR);
-	}
-	return combine(resL, resR);
-}
+    A query(z l, z rex) {
+        A la = e, ra = e;
+        for (l += n, rex += n; l < rex; l /= 2, rex /= 2) {
+            if (l&1) la = agg(la, v[l++]);
+            if (rex&1) ra = agg(v[--rex], ra);
+        }
+        return agg(la, ra);
+    }
 
-// Oder: Intervall-Modifikation, Punkt-Query:
-void modify(int l, int r, ll val) {
-	for (l += sz(tree)/2, r += sz(tree)/2; l < r; l /= 2, r /= 2) {
-		if (l&1) {tree[l] = combine(tree[l], val); l++;};
-		if (r&1) {--r; tree[r] = combine(tree[r], val);};
-}}
+    @\green{Range updates, Point queries}@
+    void update(z l, z rex, A a) {
+        for (l += n, rex += n; l < rex; l /= 2, rex /= 2) {
+            if (l&1) {v[l] = agg(v[l], a), l++;}
+            if (rex&1) {--rex, v[rex] = agg(v[rex], a);}
+        }
+    }
 
-ll query(int i) {
-	ll res = neutral;
-	for (i += sz(tree)/2; i > 0; i /= 2) {
-		res = combine(res, tree[i]);
-	}
-	return res;
-}
+    A query(z i) {
+        A r = v[i += n];
+        while(i /= 2)
+            r = agg(r, v[i]);
+        return r;
+    }
+};
