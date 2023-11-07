4e49f47dec283220fbf5f9a1dd90479a0cf8b7ed
florian <uwnte@student.kit.edu>
2023-11-07 03:42:15 +0100

    new lazyPropagation (with comments)

--- datastructures/lazyPropagation.cpp
+++ datastructures/lazyPropagation.cpp
@@ -1,87 +1,87 @@
-// TODO: Use own persistent implicit SegTree instead
-template<typename T = ll, typename U = ll>
-struct SegTree {
-	int size, height;
-	static constexpr T neutral = 0; // Neutral element for combine
-	static constexpr U updateFlag = 0; // Unused value by updates
-	vector<T> tree; vector<U> lazy;
+struct segT {
+    z sz; @\redBox{always initialize!}@
 
-	SegTree(const vector<T>& a) : SegTree(sz(a)) {
-		copy(all(a), tree.begin() + size);
-		for (int i = size - 1; i > 0; i--)
-			tree[i] = combine(tree[2 * i], tree[2 * i + 1]);
-	}
+    using A=z;
+    using U=z;
+    A e = 0;
+    U id = 0;
+    A aggr(A a, A b) {return a+b;}
+    A app(A a, U u, z s) {return a+u*s;}
+    U comp(U later, U first) {return later+first;}
 
-	SegTree(int n) : size(n), height(__lg(2 * n)),
-		tree(2 * n, neutral), lazy(n, updateFlag) {}
+    struct node {
+        int32_t l, r;
+        A agg; //or value (for leaves)
+        U lazy;
+    };
+    @\yellow{vector<node> v=\{\{0, 0, e, id\}\};}@
+    @\orange{shared\_ptr<vector<node\\>\\> v\{new vector<node>(\{0, 0, e, id\})\};}@ //Array wegen Speicher (int32_t < node*). Is that worth it???
+                                                                                     //shared_ptr: bei vielen Updates/SegTs ist es wichtig, dass der Speicher für v wieder freigegeben wird
+@\orange{\#define v (*v)}@
+#define N v[n]
+    z root = 0;
 
-	T combine(T a, T b) {return a + b;} // Modify this + neutral
+    z set(z n, node x) {
+        return @\yellow{n ? v[n]=x, n : }@(v.push_back(x), v.size()-1);
+    }
+    z apply(z n, U u, z rangesize) {
+        return set(n, {N.l, N.r, app(N.agg, u, rangesize), comp(u, N.lazy)});
+    }
+    z update(z a, z bex, z l, z rex, z n, U u) { //TODO: make update and apply return nodes (that need to be pushed into v manually, if desired)!!?
+        if(bex <= l || rex <= a) return n;
+        if(a <= l && rex <= bex) return apply(n, u, rex - l);
+        z m = (l + rex) / 2;
+        z nl = update(a, bex, l, m, apply(N.l, N.lazy, m - l), u); //here, it's important that v[0] is an empty node
+        z nr = update(a, bex, m, rex, apply(N.r, N.lazy, rex - m), u);
+        return set(n, {nl, nr, aggr(v[nl].agg, v[nr].agg)});
+    }
+    A query(z a, z bex, z l, z rex, z n) {
+        if(bex <= l || rex <= a) return e;
+        if(a <= l && rex <= bex) return N.agg;
+        //push(n, rex - l); //might need to much memory if more queries than updates allowed
+        z m = (l + rex) / 2;
+        A la = query(a, bex, l, m, N.l), ra = query(a, bex, m, rex, N.r);
+        return app(aggr(la, ra), N.lazy, min(bex, rex) - max(a, l));
+    }
 
-	void apply(int i, U val, int k) { // And this + updateFlag
-		tree[i] = val * k;
-		if (i < size) lazy[i] = val; // Don't forget this
-	}
+    z bin_search(z a, z bex, z l, z rex, z n, A &cur_agg, U lazy, function<bool(A&)> &p@\opt{, bool rtl}@) { @\optAnn@
+        if(bex <= l || rex <= a) return -1; //todo: bei Link anders -- passt das so (immer)? Chris fragen!?
+        A n_agg = app(N.agg, lazy, rex - l);
+        A new_agg = @\opt{rtl ? aggr(n\_agg, cur\_agg) : }@aggr(cur_agg, n_agg);
+        if (a <= l && rex <= bex && !p(new_agg)) return cur_agg = new_agg, -1;
+        if (l + 1 == rex) return l;
 
-	void push_down(int i, int k) {
-		if (lazy[i] != updateFlag) {
-			apply(2 * i, lazy[i], k);
-			apply(2 * i + 1, lazy[i], k);
-			lazy[i] = updateFlag;
-	}}
+        //push(n, rex - l); //might need to much memory if more queries than updates allowed
+        z m = (l + rex) / 2;
+        z res = bin_search(a, bex, @\opt{rtl?m:}@l, @\opt{rtl?rex:}@m, @\opt{rtl?N.r:}@N.l, cur_agg, lazy = comp(lazy, N.lazy), p, rtl);
+        if (res != -1) return res;
+        return bin_search(a, bex, @\opt{rtl?l:}@m, @\opt{rtl?m:}@rex, @\opt{rtl?N.l:}@N.r, cur_agg, lazy, p, rtl);
+    }
+#undef N
+@\orange{\#undef v}@
 
-	void push(int i) {
-		for (int s = height, k = 1 << (height-1); s > 0; s--, k /= 2)
-			push_down(i >> s, k);
-	}
+    @\yellow{void}\orange{segT}@ update(z l, z rex, U u) {
+        @\opt{assert(0 <= l \&\& l <= rex \&\& rex <= sz);}@
+        @\yellow{this->root = update(l, rex, 0, sz, root, u);}@
+        @\orange{return segT\{sz, e, id, v, update(l, rex, 0, sz, root, u)\};}@
+    }
 
-	void build(int i) {
-		for (int k = 2; i /= 2; k *= 2) {
-			push_down(i, k / 2);
-			tree[i] = combine(tree[2 * i], tree[2 * i + 1]);
-	}}
+    A query(z l, z rex) {
+        @\opt{assert(0 <= l \&\& l <= rex \&\& rex <= sz);}@
+        return query(l, rex, 0, sz, root);
+    }
 
-	void update(int l, int r, U val) { // data[l..r) = val
-		l += size, r += size;
-		int l0 = l, r0 = r;
-		push(l0), push(r0 - 1);
-		for (int k = 1; l < r; l /= 2, r /= 2, k *= 2) {
-			if (l&1) apply(l++, val, k);
-			if (r&1) apply(--r, val, k);
-		}
-		build(l0), build(r0 - 1);
-	}
-
-	T query(int l, int r) { // sum[l..r)
-		l += size, r += size;
-		push(l), push(r - 1);
-		T resL = neutral, resR = neutral;
-		for (; l < r; l /= 2, r /= 2) {
-			if (l&1) resL = combine(resL, tree[l++]);
-			if (r&1) resR = combine(tree[--r], resR);
-		}
-		return combine(resL, resR);
-	}
-
-	// Optional:
-	ll lower_bound(int l, int r, T x) {
-		l += size, r += size;
-		push(l), push(r - 1);
-		vector<pair<int, int>> a, st;
-		for (int k = 1; l < r; l /= 2, r /= 2, k *= 2) {
-			if (l&1) a.emplace_back(l++, k);
-			if (r&1) st.emplace_back(--r, k);
-		}
-		a.insert(a.end(), st.rbegin(), st.rend());
-		for (auto [i, k] : a) {
-			if (tree[i] >= x) return find(i, x, k); // Modify this
-		}
-		return -1;
-	}
-
-	ll find(int i, T x, int k) {
-		if (i >= size) return i - size;
-		push_down(i, k / 2);
-		if (tree[2*i] >= x) return find(2 * i, x, k / 2); // And this
-		else return find(2 * i + 1, x, k / 2);
-	}
+    //todo: unschön (siehe auch treap): wenn schon p(neutralE), wird l zurückgegeben
+    //left_to_right:
+    //  returns smallest k in [l, rex) so that p(agg(a_l, ..., a_k)), or -1 if impossible
+    //  p should be increasing, i.e.  p(a1) not less than p(agg(a1, a2))
+    //right_to_left:
+    //  returns largest k in [l, rex) so that p(agg(a_k, ..., a_rex-1)), or -1 if impossible
+    //  p should be decreasing, i.e.  p(a1) not greater than p(agg(a1, a2))
+    z bin_search(z l, z rex, function<bool(A&)> p, bool right_to_left = false) { @\optAnn@
+        @\opt{assert(0 <= l \&\& l <= rex \&\& rex <= sz);}@
+        if(l==rex) return -1;
+        A curr_agg = e;
+        return bin_search(l, rex, 0, sz, root, curr_agg, id, p, right_to_left);
+    }
 };
