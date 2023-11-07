04741d9c16368b70b2563667b6a5d9c7f4090c7b
florian <uwnte@student.kit.edu>
2023-11-07 17:41:26 +0100

    del cmmts

--- datastructures/segT.cpp
+++ datastructures/segT.cpp
@@ -13,11 +13,9 @@ struct segT {
         int32_t l, r;
         A agg; //or value (for leaves)
         U lazy;
-    };    @\greenBox{not persistent}@ @\yellowBox{persistent}@
+    };    @\greenBox{not persistent}@  @\yellowBox{persistent}@
     @\green{vector<node> v = {{0, 0, e, id}};}@
     @\yellow{shared_ptr<vector<node>> v{new vector<node>{{0, 0, e, id}}};}@
-    //Array wegen Speicher (int32_t < node*). Is that worth it??? // Linus: probably this is also faster because of caching
-    //shared_ptr: bei vielen Updates/SegTs ist es wichtig, dass der Speicher für v wieder freigegeben wird
 @\yellow{#define v (*v)}@
 #define N v[n]
     z root = 0;
@@ -28,18 +26,17 @@ struct segT {
     z apply(z n, U u, z rangesize) {
         return set(n, {N.l, N.r, app(N.agg, u, rangesize), comp(u, N.lazy)});
     }
-    z update(z a, z bex, z l, z rex, z n, U u) { //TODO: make update and apply return nodes (that need to be pushed into v manually, if desired)!!?
+    z update(z a, z bex, z l, z rex, z n, U u) {
         if(bex <= l || rex <= a) return n;
         if(a <= l && rex <= bex) return apply(n, u, rex - l);
         z m = (l + rex) / 2;
-        z nl = update(a, bex, l, m, apply(N.l, N.lazy, m - l), u); //here, it's important that v[0] is an empty node
+        z nl = update(a, bex, l, m, apply(N.l, N.lazy, m - l), u);
         z nr = update(a, bex, m, rex, apply(N.r, N.lazy, rex - m), u);
         return set(n, {nl, nr, aggr(v[nl].agg, v[nr].agg)});
     }
     A query(z a, z bex, z l, z rex, z n) {
         if(bex <= l || rex <= a) return e;
         if(a <= l && rex <= bex) return N.agg;
-        //push(n, rex - l); //might need to much memory if more queries than updates allowed
         z m = (l + rex) / 2;
         A la = query(a, bex, l, m, N.l), ra = query(a, bex, m, rex, N.r);
         return app(aggr(la, ra), N.lazy, min(bex, rex) - max(a, l));
@@ -47,13 +44,11 @@ struct segT {
 
     z lower_bound(z a, z bex, z l, z rex, z n, @\optAnn@
            A &cur_agg, U lazy, function<bool(A&)> &p@\opt{, bool rtl}@) {
-        if(bex <= l || rex <= a) return -2; //todo: bei Link anders -- passt das so (immer)? Chris fragen!?
+        if(bex <= l || rex <= a) return -2;
         A n_agg = app(N.agg, lazy, rex - l);
         A new_agg = @\opt{rtl ? aggr(n_agg, cur_agg) : }@aggr(cur_agg, n_agg);
         if (a <= l && rex <= bex && !p(new_agg)) return cur_agg = new_agg, -2;
         if (l + 1 == rex) return l;
-
-        //push(n, rex - l); //might need to much memory if more queries than updates allowed
         z m = (l + rex) / 2;
         z res = lower_bound(a, bex, @\opt{rtl?m:}@l, @\opt{rtl?rex:}@m, @\opt{rtl?N.r:}@N.l, cur_agg, lazy = comp(lazy, N.lazy), p, rtl);
         if (res != -2) return res;
