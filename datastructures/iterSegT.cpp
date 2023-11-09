4e6774bee63f488bde5c943e415c97c19ec705d8
florian <uwnte@student.kit.edu>
2023-11-09 15:41:38 +0100

    shrink

--- datastructures/iterSegT.cpp
+++ datastructures/iterSegT.cpp
@@ -16,8 +16,7 @@ struct iterSegT {
     @\greenBox{Point updates, Range queries}@
     void set(z i, A a) {
         v[i += n] = a;
-        while (i /= 2)
-            v[i] = agg(v[2 * i], v[2 * i + 1]);
+        while (i /= 2) v[i] = agg(v[2 * i], v[2 * i + 1]);
     }
 
     A query(z l, z rex) {
@@ -34,13 +33,10 @@ struct iterSegT {
         for (l += n, rex += n; l < rex; l /= 2, rex /= 2) {
             if (l&1) {v[l] = agg(v[l], a), l++;}
             if (rex&1) {--rex, v[rex] = agg(v[rex], a);}
-        }
-    }
+    }}
 
     A query(z i) {
         A r = v[i += n];
-        while(i /= 2)
-            r = agg(r, v[i]);
+        while(i /= 2) r = agg(r, v[i]);
         return r;
-    }
-};
+}};
