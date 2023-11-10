5b16cb97665bf36aefa864744b4c8a544b77e9d9
florian <uwnte@student.kit.edu>
2023-11-10 23:59:40 +0100

    new union_find

--- datastructures/unionFind.cpp
+++ datastructures/unionFind.cpp
@@ -1,18 +1,29 @@
-#pragma once
-#include "./header.h"
-
-/// O(alpha(n))
 struct union_find {
-    vz e; //<0: -(maximum possible) height @\yellow{size}@, else: parent
+    vz e; //<0: -(maximum possible) height/@\yellow{size}@, else: parent
+    @\green{vpzz st{};}@
+
     union_find(z sz) : e(sz, -1) {}
+
     z find(z a) {return e[a] < 0 ? a : e[a] = find(e[a]);}
+
     void unite(z a, z b) {
         a = find(a), b = find(b);
         if (a == b) return;
         if (e[a] > e[b]) swap(a, b);
-        e[a] -= @\yellow{e[a] == }@e[b]; @\yellowBox{for storing+uniting by size(slower)}@
+        @\green{st.emplace_back(a, e[a]);}@
+        @\green{st.emplace_back(b, e[b]);}@
+        e[a] -= e[a] == e[b]; @\yellowBox{by size: instead e[a]+=e[b] (slower)}@
         e[b] = a;
     }
+
     bool same(z a, z b) {return find(a) == find(b);}
+
     @\yellow{z sz(z a) {return -e[find(a)];}}@
+
+    @\green{z time() {return st.size();}}@
+
+    @\green{void rollback(z time) }\greenE\{@
+    @\greenE\Quad\green{fe([a, x]:vpzz(time + st.begin(), st.end())) e[a] = x;}@
+    @\greenE\Quad\green{st.resize(time);}@
+    @\greenE\}@
 };
