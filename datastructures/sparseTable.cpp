60cc4f96f0c8aa285a940adb27f7ae1e3780890f
Linus <linuscodes@scobe.de>
2023-11-11 19:11:03 +0100

    improve sparse table

--- datastructures/sparseTable.cpp
+++ datastructures/sparseTable.cpp
@@ -1,23 +1,23 @@
 struct SparseTable {
-	vector<vector<int>> st;
-	vector<ll> *a;
+	z n; vz a; vvz st;
 
-	int better(int lidx, int ridx) {
-		return a[lidx] <= a[ridx] ? lidx : ridx;
+	// @\orangeBox{config}@  default: min
+	z better(z l, z r) { // idempotent, indices!
+		return a[l] @\orange{<=}@ a[r] ? l : r; 
 	}
-
-	void init(vector<ll> *vec) {
-		int n = sz(*vec);
-		a = vec->data();
-		st.assign(__lg(n) + 1, vector<int>(n));
-		iota(all(st[0]), 0);
-		for (int j = 0; (2 << j) <= n; j++) {
-			for (int i = 0; i + (2 << j) <= n; i++) {
-				st[j + 1][i] = better(st[j][i] , st[j][i + (1 << j)]);
+	
+	SparseTable(vz &vec): 
+		n(vec.size()), a(vec), st(__lg(n)+1, vz(n)) {
+		iota(be(st[0]), 0);
+		for (z j = 0; (2 << j) <= n; j++) {
+			for (z i = 0; i + (2 << j) <= n; i++) {
+				st[j + 1][i] = better(st[j][i], st[j][i + (1 << j)]);
 	}}}
 
-	int queryIdempotent(int l, int r) {
-		int j = __lg(r - l); //31 - builtin_clz(r - l);
-		return better(st[j][l] , st[j][r - (1 << j)]);
+	z queryIndex(z l, z rex) {
+		@\opt{assert(0 <= l && l <= rex && rex <= n);}@ // rex>n => UB
+		@\optE{if (l == rex) return \orange{INF};}@ // neutrales Element
+		z j = __lg(rex - l);
+		return better(st[j][l], st[j][rex - (1 << j)]);
 	}
 };
