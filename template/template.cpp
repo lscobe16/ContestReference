b168b14a44a8019cb4a260bc4f048a50334357c4
florian <uwnte@student.kit.edu>
2023-11-11 19:17:43 +0100

    new template

--- template/template.cpp
+++ template/template.cpp
@@ -1,19 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define tsolve int t; cin >> t; while(t--) solve
-#define debug(x) cerr << __LINE__ << ": "#x" = " << (x) << endl
-#define nl '\n'
-#define all(x) ::begin(x), ::end(x)
-#define sz(x) (ll)::size(x)
+#define int long long
 
-using ll = long long;
-using ld = long double;
- 
-void solve() {}
- 
-int main() {
-	cin.tie(0)->sync_with_stdio(false);
-	cout << setprecision(16);
-	solve();
+using z = int;
+using vz = vector<z>;
+@\opt{using vvz = vector<vz>;}@
+@\optE{// auch mit\ \ pzz=pair<z,z>\ \ und\ \ ld=long double\ \ (mit Alt+J)}@
+
+#define au auto&&
+#define fe(x...) for(au x)
+
+// Usage: z in(n, m)    and    vz inv(a, n)
+#define in(x...) x; [](au... a){((cin>>a), ...);}(x);
+#define inv(v, n) v(n); fe(_:v){in(_)}
+
+@\opt{au pr = cout;}@
+au operator-(ostream& o, au t) {return o << t << ' ';}
+au operator+(ostream& o, au t) {return o << t << '\n';}
+
+@\opt{z INF = 1e18;}@ //max z ca= 9e18
+
+@\optE{\#define dbg(x) (cerr<<\_\_LINE\_\_<<": \textbackslash}\optE{t"(\#x+1)" = ")x;}@
+
+int32_t main() {
+    @\opt{cin.tie(0)->sync_with_stdio(0);}@
+    @\opt{cout.precision(16);}@
+
 }
