6bb8c0c467ea93a0e370b12f5a2a1a9eb5039ddb
Linus <linuscodes@scobe.de>
2023-11-11 18:59:12 +0100

    adopt convexHull

--- geometry/convexHull.cpp
+++ geometry/convexHull.cpp
@@ -1,15 +1,15 @@
 vector<pt> convexHull(vector<pt> pts){
-	sort(all(pts), [](au a, au b){
+	sort(be(pts), [](au a, au b){
         return pair{a.real(), a.imag()} < pair{b.real(), b.imag()};
 	});
-	pts.erase(unique(all(pts)), pts.end());
+	pts.erase(unique(be(pts)), @\red{pts.end()}@);
 	int k = 0;
-	vector<pt> h(2 * sz(pts));
-	for (int i = 0; i < sz(pts); i++) {// Untere Hülle.
+	vector<pt> h(2 * pts.size());
+	for (int i = 0; i < pts.size(); i++) {// Untere Hülle
 		while (k > 1 && cross(h[k-2], h[k-1], pts[i]) <= 0) k--;
 		h[k++] = pts[i];
 	}
-	for (int i = sz(pts)-2, t = k; i >= 0; i--) {// Obere Hülle.
+	for (int i = pts.size()-2, t = k; i >= 0; i--) {// Obere Hülle
 		while (k > t && cross(h[k-2], h[k-1], pts[i]) <= 0) k--;
 		h[k++] = pts[i];
 	}
