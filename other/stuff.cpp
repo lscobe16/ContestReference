91d6cc712eb67786bf3cce3ddff8f57e1791f40e
Linus <linuscodes@scobe.de>
2023-11-14 12:39:57 +0100

    Generelles Zeug: Strategie, Practice, Aussortieren

--- other/stuff.cpp
+++ other/stuff.cpp
@@ -1,19 +1,8 @@
-// Alles-Header.
-#include <bits/stdc++.h>
-
 // Setzt das deutsche Tastaturlayout.
 setxkbmap de
 
-// Schnelle Ein-/Ausgabe mit cin/cout.
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-
 // Set mit eigener Sortierfunktion.
 set<point2, decltype(comp)> set1(comp);
-
-// STL-Debugging, Compiler flags.
--D_GLIBCXX_DEBUG
-#define _GLIBCXX_DEBUG
 
 // 128-Bit Integer/Float. Muss zum Einlesen/Ausgeben
 // in einen int oder long long gecastet werden.
@@ -22,8 +11,3 @@ __int128, __float128
 // float mit Decimaldarstellung
 #include <decimal/decimal>
 std::decimal::decimal128
-
-// 1e18 < INF < Max_Value / 2
-constexpr ll INF = 0x3FFF'FFFF'FFFF'FFFFll;
-// 1e9 < INF < Max_Value / 2
-constexpr int INF = 0x3FFF'FFFF;
--- template/console.cpp
+++ template/console.cpp
@@ -2,5 +2,5 @@
 alias mygpp="g++ -std=gnu++20 -x c++ -Wall -O2 -static -pipe -o $1 \"$1.cpp\" -lm"
 
 alias comp="g++ -std=gnu++20 -O2 -Wall -Wextra -Wshadow -Wconversion"
-alias dbg="comp -g -fsanitize=address,undefined"
+alias dbg="comp -g -fsanitize=address,undefined -D_GLIBCXX_DEBUG"
 // use    comp main.cpp && ./a.out
