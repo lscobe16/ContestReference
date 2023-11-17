cb4f8e16d4fb654c04763e4a372f37c8c8c566ba
florian <uwnte@student.kit.edu>
2023-11-17 14:45:17 +0100

    move lca labels

--- graph/LCA_sparse_compiling.cpp
+++ graph/LCA_sparse_compiling.cpp
@@ -16,12 +16,12 @@ struct LCA {
 	SparseTable *st; // -> 1.5
 
     // computes @\greenBox{euler tour}@
-	void dfs(vvz& G, z v, z p=-1, z d=0) {
-		t2n[idx] = v, n2t[v] = idx, dep[idx++] = d;
-		for (z u : G[v]) if (u!=p) {
-            dfs(G, u, v, d+1);
-            t2n[idx] = v, dep[idx++] = d;
-	}}
+	void dfs(vvz& G, z u, z p=-1, z d=0) {
+		t2n[idx] = u, n2t[u] = idx, dep[idx++] = d;
+		for (z v : G[u]) if (v!=p) {
+				dfs(G, v, u, d+1);
+				t2n[idx] = u, dep[idx++] = d;
+			}}
 	LCA(vvz& G, z r=0): n(G.size()), dep(2*n), t2n(2*n,-1), n2t(n, 2*n) {
 		dfs(G, r);
 		st = new SparseTable(dep);
