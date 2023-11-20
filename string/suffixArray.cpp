a77e8f0624673afeef24922f8b842b04f88b5623
Linus <linuscodes@scobe.de>
2023-11-20 21:16:21 +0100

    improve SuffixArray

--- string/suffixArray.cpp
+++ string/suffixArray.cpp
@@ -1,31 +1,30 @@
 struct SuffixArray {
-	int n, step, count;
-	vector<int> SA, LCP;
-	vector<vector<int>> P;
-	vector<pair<pair<int, int>, int>> L;
+	z n, step, count; // SA[i] = start index of i-smallest suffix
+	vz SA, LCP; // LCP[i] = lcp(SA[i - 1], SA[i])
+	vvz P;
+	vector<pair<pair<z, z>, z>> L;
 
-	SuffixArray(const string& s) : n(sz(s)), SA(n), LCP(n), L(n) {
-		P.assign(__lg(n)+2, vector<int>(n));
-		for (int i = 0; i < n; i++) P[0][i] = s[i];
+	SuffixArray(string& s) : n(s.size()), SA(n), LCP(n), L(n) {
+		P = vvz(__lg(n)+2, vz(n));
+		for (z i = 0; i < n; i++) P[0][i] = s[i];
 		for (step = 1, count = 1; count < n; step++, count *= 2) {
-			for (int i = 0; i < n; i++)
+			for (z i = 0; i < n; i++)
 				L[i] = {{P[step-1][i],
 				         i+count < n ? P[step-1][i+count] : -1}, i};
-			sort(all(L));
-			for (int i = 0; i < n; i++) {
+			sort(L.begin(), L.end());
+			for (z i = 0; i < n; i++) {
 				P[step][L[i].second] =
 					i > 0 && L[i].first == L[i-1].first ?
 					P[step][L[i-1].second] : i;
 		}}
-		for (int i = 0; i < n; i++) SA[i] = L[i].second;
-		for (int i = 1; i < n; i++) LCP[i] = lcp(SA[i - 1], SA[i]);
+		for (z i = 0; i < n; i++) SA[i] = L[i].second;
+		for (z i = 1; i < n; i++) LCP[i] = lcp(SA[i - 1], SA[i]);
 	}
 
-	// x and y are text-indices, not SA-indices.
-	int lcp(int x, int y) {
-		int ret = 0;
+	z lcp(z x, z y) { // x and y are text-indices, not SA
+		z ret = 0;
 		if (x == y) return n - x;
-		for (int k = step - 1; k >= 0 && x < n && y < n; k--) {
+		for (z k = step - 1; k >= 0 && x < n && y < n; k--) {
 			if (P[k][x] == P[k][y]) {
 				x += 1 << k;
 				y += 1 << k;
