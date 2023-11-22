a1013dbc1602820f98d9584f2750e3bcaa0b0bc9
Linus <linuscodes@scobe.de>
2023-11-22 11:43:12 +0100

    mygpp as function

--- template/console.bash
+++ template/console.bash
@@ -1,6 +1,6 @@
-// mygpp is what the judge uses
-alias mygpp="g++ -std=gnu++20 -x c++ -Wall -O2 -static -pipe -o $1 \"$1.cpp\" -lm"
-
 alias comp="g++ -std=gnu++20 -O2 -Wall -Wextra -Wshadow -Wconversion"
 alias dbg="comp -g -fsanitize=address,undefined -D_GLIBCXX_DEBUG"
 // use    comp main.cpp && ./a.out    < "1.in" | diff "1.out" -
+
+// mygpp is what the judge uses
+function mygpp {g++ -std=gnu++20 -x c++ -Wall -O2 -static -pipe -o $1 \"$1.cpp\" -lm;}
