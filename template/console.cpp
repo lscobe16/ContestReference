0ca783f7ab438f9864487a16b54f86dd004653b4
Linus <linuscodes@scobe.de>
2023-11-22 10:59:58 +0100

    add diff to console

--- template/console.cpp
+++ template/console.cpp
@@ -3,4 +3,4 @@ alias mygpp="g++ -std=gnu++20 -x c++ -Wall -O2 -static -pipe -o $1 \"$1.cpp\" -l
 
 alias comp="g++ -std=gnu++20 -O2 -Wall -Wextra -Wshadow -Wconversion"
 alias dbg="comp -g -fsanitize=address,undefined -D_GLIBCXX_DEBUG"
-// use    comp main.cpp && ./a.out
+// use    comp main.cpp && ./a.out    < "1.in" | diff "1.out" -
