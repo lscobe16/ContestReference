a082e46e707651e402fff55e2311df202e7a4769
Linus <linuscodes@scobe.de>
2023-11-22 16:08:45 +0100

    rework rollingHashes

--- string/rollingHash.cpp
+++ string/rollingHash.cpp
@@ -1,16 +1,16 @@
-// q = 29, 53, 101, 257, 1009, 65'537
-// or choose q random from [sigma, m)
-// m = 1'500'000'001, 1'600'000'009, 1'700'000'009
-template<ll M, ll Q>
-struct Hasher {
-	vector<ll> power = {1}, pref = {0};
+z M = 1'500'000'001; // 1'600'000'009, 1'700'000'009
+z Q = 101; // 29, 53, 257, 1009, 65'537 \in [max(alphabet), M]
+char A = 'A';
+@\green{template<z M, z Q>}@  @\greenBox{multiple different bases}@
+struct Hasher { // bigger powers are left
+	vector<z> power = {1}, pref = {0};
 	Hasher(const string& s) {
 		for (auto x : s) {
 			power.push_back(power.back() * Q % M);
-			pref.push_back((pref.back() * Q % M + x) % M);
+			pref.push_back((pref.back() * Q % M + (x-A+1)) % M);
 	}}
 
-	ll hash(int l, int r) {	// Berechnet hash(s[l..r)).
-		return (pref[r] - power[r-l] * pref[l] % M + M) % M;
+	z hash(z l, z rex) {	// Berechnet hash(s[l..rex)).
+		return (pref[rex] - power[rex-l] * pref[l] % M + M) % M;
 	}
 };
