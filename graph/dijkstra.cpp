36ac779f8ff0c27363a2dbdc4adfb76f542be8e4
florian <uwnte@student.kit.edu>
2023-11-23 06:00:07 +0100

    fix dijkstra... ^^

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,12 +1,11 @@
 // adj[i] = {(j, c(e)) | (i, j) = e in E}
-using pzz = pair<z, z>; //dist, destination
 @\green{pair<}@vz@\green{, vz>}@ dijkstra(vvpzz& adj, z start) {
 	priority_queue<pzz, vpzz, greater<pzz>> Q;
 	vz dist(adj.size(), INF)@\green{, prev(adj.size(), -1)}@;
 	dist[start] = 0; Q.emplace(0, start);
 
 	while (Q.size()) {
-		auto [du, u] = Q.top(); Q.pop();
+		au@\red{to}@ [du, u] = Q.top(); Q.pop();
 		if (du > dist[u]) continue; // lazy decreaseKey
 		fe([v, c] : adj[u]) {
 			if (du + c < dist[v]) {
@@ -17,6 +16,6 @@ using pzz = pair<z, z>; //dist, destination
 	return @\greenE{\{}@dist@\greenE{, prev\}}@;
 }
 
-@\green{vz path = {{t}}; z c = t;}@
+@\green{vz path = {t}; z c = t;}@
 @\green{while (c != s) path.push_back(c = prev[c]);}@
 @\green{reverse(path.begin(), path.end());}@
