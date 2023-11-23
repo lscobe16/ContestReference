cf62aaea6cf8f7a390adab153f4f276c4ce53d8b
florian <uwnte@student.kit.edu>
2023-11-23 03:51:38 +0100

    make PolyRolyHash + doc a bit better

--- string/rollingHash.cpp
+++ string/rollingHash.cpp
@@ -1,11 +1,11 @@
 z M = 1'500'000'001; // 1'600'000'009, 1'700'000'009
-z Q = 101; // 29, 53, 257, 1009, 65'537 \in [max(alphabet), M]
+z Q = 101; // 29, 53, 257, 1009, 65'537 \in [max(alphabet) /*TODO: +1 or +2?*/, M]
 char A = 'A';
 @\green{template<z M, z Q>}@  @\greenBox{multiple different bases}@
 struct Hasher { // bigger powers are left
-	vector<z> power = {1}, pref = {0};
-	Hasher(const string& s) {
-		for (auto x : s) {
+	vz power = {1}, pref = {0};
+	Hasher(string s) {
+		fe (x:s) {
 			power.push_back(power.back() * Q % M);
 			pref.push_back((pref.back() * Q % M + (x-A+1)) % M);
 	}}
