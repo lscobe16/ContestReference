2682e55955dd6809afeda0e9430763b8831fcbc1
florian <uwnte@student.kit.edu>
2023-11-23 05:10:31 +0100

    coco fix typo

--- other/coordCompression.cpp
+++ other/coordCompression.cpp
@@ -2,5 +2,5 @@ vz A; // zu komprimierende Werte
 set<z> as(A.begin(), A.end());
 vz ix2a(as.begin(), as.end());
 map<z, z> a2ix;
-for (z i = 0; i < ix2t.size(); i++) a2ix[ix2t[i]] = i;
+for (z i = 0; i < ix2a.size(); i++) a2ix[ix2a[i]] = i;
 @\opt{fe (a:A) a = a2ix[a];}@ // alle Vorkommen ersetzen
