674871b8f5a4693d71857ee3440bcde38505bb0e
Linus <linuscodes@scobe.de>
2024-01-21 16:58:02 +0100

    improve Gauss (untested)

--- math/gauss.cpp
+++ math/gauss.cpp
@@ -1,36 +1,43 @@
-void normalLine(int line) {
-	double factor = mat[line][line];
-	for (double& x : mat[line]) x /= factor;
+z gauss(vvld& mat) {
+    z n = mat.size();
+	vb done(n, false);
+	for (z l = 0; l < n; l++) {
+		z swappee = l; // more stability
+		for (z j = 0; j < n; j++) {
+			if (done[j]) continue;
+			if (abs(mat[j][l]) > abs(mat[l][l])) swappee = j;
+		}
+		swap(mat[l], mat[swappee]);
+		if (abs(mat[l][l]) > EPS) {
+            ld factor = mat[l][l];
+            for (ld& x : mat[l]) x /= factor;
+            for (z i = 0; i < n; i++) {
+                if (i == l) continue;
+                ld diff = mat[i][l];
+                for (z j = 0; j < mat[0].size(); j++) {
+                    mat[i][j] -= diff * mat[l][j];
+            }}
+			done[i] = true;
+	}}
+	for (z i = 0; i < n; i++) { // checks
+		bool allZero = true;
+		for (z j = i; j < n; j++) allZero &= abs(mat[i][j]) <= EPS;
+		if (allZero && abs(mat[i][n]) > EPS) return 0; // no solution
+		if (allZero && abs(mat[i][n]) <= EPS) return 2; // many solutions
+	}
+    return 1;
 }
 
-void takeAll(int n, int line) {
-	for (int i = 0; i < n; i++) {
-		if (i == line) continue;
-		double diff = mat[i][line];
-		for (int j = 0; j <= n; j++) {
-			mat[i][j] -= diff * mat[line][j];
-}}}
-
-int gauss(int n) {
-	vector<bool> done(n, false);
-	for (int i = 0; i < n; i++) {
-		int swappee = i; // Sucht Pivotzeile für bessere Stabilität.
-		for (int j = 0; j < n; j++) {
-			if (done[j]) continue;
-			if (abs(mat[j][i]) > abs(mat[i][i])) swappee = j;
-		}
-		swap(mat[i], mat[swappee]);
-		if (abs(mat[i][i]) > EPS) {
-			normalLine(i);
-			takeAll(n, i);
-			done[i] = true;	
-	}}
-	// Ab jetzt nur checks bzgl. Eindeutigkeit/Existenz der Lösung.
-	for (int i = 0; i < n; i++) {
-		bool allZero = true;
-		for (int j = i; j < n; j++) allZero &= abs(mat[i][j]) <= EPS;
-		if (allZero && abs(mat[i][n]) > EPS) return INCONSISTENT;
-		if (allZero && abs(mat[i][n]) <= EPS) return MULTIPLE;
+void invert(vvld& A) {
+    assert(A.size() == A[0].size());
+    z n = A.size();
+	vvld AB(n, vld(2*n));
+    for (z i = 0; i < n; i++) AB[i][b+i] = 1;
+	for (z i = 0; i < n; i++) {
+		for (z j = 0; j < n; j++) AB[i][j] = A[i][j];
 	}
-	return UNIQUE;
+	assert(gauss(AB) == 1);
+	for (z i = 0; i < n; i++) {
+		for (z j = 0; j < n; j++) A[i][j] = AB[i][n+j];
+	}
 }
--- math/lgsFp.cpp
+++ math/lgsFp.cpp
@@ -1,26 +1,18 @@
-void normalLine(int line, ll p) {
-	ll factor = multInv(mat[line][line], p);
-	for (ll& x : mat[line]) x = (x * factor) % p;
-}
-
-void takeAll(int n, int line, ll p) {
-	for (int i = 0; i < n; i++) {
-		if (i == line) continue;
-		ll diff = mat[i][line];
-		for (int j = 0; j <= n; j++) {
-			mat[i][j] -= (diff * mat[line][j]) % p;
-			mat[i][j] = (mat[i][j] + p) % p;
-}}}
-
-void gauss(int n, ll mod) {
-	vector<bool> done(n, false);
-	for (int i = 0; i < n; i++) {
-		int j = 0;
-		while (j < n && (done[j] || mat[j][i] == 0)) j++;
+void gauss(vvz& mat, z mod) {
+	z n = mat.size()
+	vb done(n, false);
+	for (z l = 0; l < n; l++) {
+		z j = 0;
+		while (j < n && (done[j] || mat[j][l] == 0)) j++;
 		if (j == n) continue;
-		swap(mat[i], mat[j]);
-		normalLine(i, mod);
-		takeAll(n, i, mod);
+		swap(mat[l], mat[j]);
+		z factor = multInv(mat[l][l], p);
+		for (z& x : mat[l]) x = (x * factor) % p;
+		for (z i = 0; i < n; i++) {
+			if (i == l) continue;
+			z diff = mat[i][l];
+			for (z j = 0; j <= n; j++) {
+				mat[i][j] -= (diff * mat[l][j] + p) % p;
+		}}
 		done[i] = true;
-}}
-// für Eindeutigkeit, Existenz etc. siehe LGS
+}} // 0/1/viele Lösungen siehe oben
